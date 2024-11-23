4609befb7ffa8f51f801d4d4e481979283c49948
IshDiaz <2024670@student.cct.ie>
2024-11-24 01:49:46 +0530

    Update healthcare.sol
    Add Record struct and records mapping

--- healthcare.sol
+++ healthcare.sol
@@ -2,5 +2,11 @@
 pragma solidity ^0.8.0;
 
 contract Healthcare {
+    struct Record {
+        string dataHash;
+        address owner;
+        mapping(address => bool) permissions;
+    }
 
+    mapping(uint => Record) public records;
 }
